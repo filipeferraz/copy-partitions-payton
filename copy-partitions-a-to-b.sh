1e716b05bb0a129d7912abdcdbcd555a6aa59321
filipepferraz <filipepferraz@gmail.com>
2018-06-11 20:55:13 -0300

    Changing to make script more generic

--- copy-partitions-a-to-b.sh
+++ copy-partitions-a-to-b.sh
@@ -1,15 +1,24 @@
 # Partitions ignored
-IGNORED=" oem_a system_a boot_a "
+IGNORED=" oem_a oem_b system_a system_b boot_a boot_b vendor_a vendor_b "
 
-for a in /dev/block/platform/soc/c0c4000.sdhci/by-name/*_a; do
-	partition=$(basename $a)
+suffix_active=$(getprop ro.boot.slot_suffix)
+
+if [[ "$suffix_active" == "_a" ]]; then 
+  suffix_swap="_b"
+else
+  suffix_swap="_a"
+fi
+
+
+for active in /dev/block/bootdevice/by-name/*$suffix_active; do
+	partition=$(basename $active)
     if [[ "$IGNORED" != *" $partition "* ]]; then
         echo "Partition $partition"
-  	    b=$(echo $a | sed 's/_a$/_b/');
-        part_a=$(readlink -fn $a);
-        part_b=$(readlink -fn $b);
-        if [[ -n "$part_a" && -n "$part_b" && "$a" != "$part_a" && "$b" != "$part_b" ]]; then
-          dd if=$part_a of=$part_b
+  	    inactive=$(echo $active | sed "s/${suffix_active}\$/${suffix_swap}/");
+        part_active=$(readlink -fn $active);
+        part_inactive=$(readlink -fn $inactive);
+        if [[ -n "$part_active" && -n "$part_active" && "$active" != "$part_active" && "$inactive" != "$part_inactive" ]]; then
+          dd if=$part_active of=$part_inactive
         fi  
     fi
 done
